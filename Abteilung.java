32db3d9a595a2fd23f3232617bd906878fb73ef9
Benjamin <benbau23@gmx.at>
2018-12-17 13:59:14 +0100

    mit abteilungen angefangen

--- Abteilung.java
+++ Abteilung.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Abteilung {
+    private String Kuerzel;
+    private String Name;
+    private ArrayList Schueler;
+
+    public String getKuerzel() {
+        return Kuerzel;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
 }
