44c0303fa12595639b1c87fc50f6133dbabc6303
Benjamin <benbau23@gmx.at>
2019-01-07 14:49:22 +0100

    Adresse fertig

--- Adresse.java
+++ Adresse.java
@@ -1,2 +1,28 @@
 public class Adresse {
+    private String ort;
+    private String strasse;
+    private int hausnummer;
+    private int plz;
+
+    public int getHausnummer() {
+        return hausnummer;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+    public void printAdresse(){
+    System.out.println("Ort: "+ort);
+    System.out.println("Strasse: "+strasse);
+    System.out.println("Hausnummer: "+hausnummer);
+    System.out.println("PLZ: "+plz);
+    }
 }
