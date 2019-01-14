1571f2b0aaa78f3f07391cc67ecaa75d8a32cdb6
Janetoilet <ssumbic@outlook.at>
2019-01-14 14:48:43 +0100

    ...

--- Belegung.java
+++ Belegung.java
@@ -1,2 +1,3 @@
 public class Belegung {
+    private int unterrichtsEinheit;
 }
--- Klasse.java
+++ Klasse.java
@@ -1,2 +1,12 @@
 public class Klasse {
+    private String bezeichnung;
+    private int schulstufe;
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public int getSchulstufe() {
+        return schulstufe;
+    }
 }
