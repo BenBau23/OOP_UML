a132b8f2c4ce0e60909620e66cf7309e984cb30d
Benjamin <benbau23@gmx.at>
2019-01-14 14:29:48 +0100

    Schueler iz ready 2go

--- Schueler.java
+++ Schueler.java
@@ -4,5 +4,28 @@ public class Schueler {
     private int katalognummer;
     private boolean eigenberechtigt;
     private Date eintrittsdatum;
-    private Klasse klasse;
+
+    public Date getEintrittsdatum() {
+        return eintrittsdatum;
+    }
+
+    public int getKatalognummer() {
+        return katalognummer;
+    }
+
+    public boolean isEigenberechtigt() {
+        return eigenberechtigt;
+    }
+
+    public void setEigenberechtigt(boolean eigenberechtigt) {
+        this.eigenberechtigt = eigenberechtigt;
+    }
+
+    public void setKatalognummer(int katalognummer) {
+        this.katalognummer = katalognummer;
+    }
+
+    public void setEintrittsdatum(Date eintrittsdatum) {
+        this.eintrittsdatum = eintrittsdatum;
+    }
 }
