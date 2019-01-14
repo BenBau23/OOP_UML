b55500f6bc9d68ea7c6721935c074cf98cf8d910
Benjamin <benbau23@gmx.at>
2019-01-14 14:08:22 +0100

    ...

--- NichtLehrpersonal.java
+++ NichtLehrpersonal.java
@@ -1,2 +1,3 @@
 public class NichtLehrpersonal extends Mitarbeiter {
+    private Schule angestellt_bei;
 }
--- Schule.java
+++ Schule.java
@@ -1,2 +1,17 @@
 public class Schule {
+    private String Name;
+    private long Schulkennzahl;
+    private String Schultyp;
+    private Adresse Standort;
+    private Schueler Schulsprecher;
+    private Abteilung Abteilungen[20];
+
+
+
+    public Abteilung addAbteilung(String n, String kuerzel) {
+        Abteilung abt;
+        abt.setName(n);
+        abt.setKuerzel(kuerzel);
+        return abt;
+    }
 }
